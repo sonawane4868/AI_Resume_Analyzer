78177e0168d7ee4c458f33c0978fb93dc3f010f8
sonawane4868 <111578023+sonawane4868@users.noreply.github.com>
2026-04-30 17:46:41 +0530

    llm changes

--- backend/gemini.py
+++ backend/gemini.py
@@ -1,8 +1,10 @@
 from google import genai
+from app.core.config import GEMINI_API_KEY
 
-client = genai.Client(api_key="AIzaSyD9LfcqFWjqpGB2Z1wnSWuC4d2YedTSazI")
+client = genai.Client(api_key=GEMINI_API_KEY)
 
 models = client.models.list()
 
 for m in models:
     print(m.name)
+
